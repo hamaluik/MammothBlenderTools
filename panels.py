3c4c37a0aae21bf4cd1bee25e9ce2a5652cd3862
Kenton Hamaluik <hamaluik@ualberta.ca>
2016-10-04 16:06:37 -0600

    re-implemented fully working panel!

--- panels.py
+++ panels.py
@@ -10,7 +10,19 @@ class MammothComponents(bpy.types.Panel):
 		layout = self.layout
 		obj = context.object
 		
-		row = layout.row()
-		row.label('blah')
-
+		for key, attributes in bpy.mammothComponentsLayout.items():
+			comp = getattr(obj, "mammoth_component_%s" % key)
+			if comp.internal___active:
+				col = layout.column()
+				row = col.row()
+				row.label(key)
+				row.operator("wm.delete_mammoth_component", text="", icon="X").component_name=key
+				
+				row = col.row()
+				for attribute in attributes:
+					col.prop(comp, attribute['name'])
+					
+				layout.separator()
+		
+		layout.operator("wm.call_menu", text="Add Component").name="OBJECT_MT_add_mammoth_component_menu"
 		layout.prop(context.scene.mammoth_components_settings, "definitions_path", text="Definitions File")
